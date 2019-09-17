49d2d11602dc370ea81714e1d32f70d0c093fc44
duongdat95 <49948339+duongdat95@users.noreply.github.com>
2019-09-18 01:15:20 +0700

    update

--- HADA/script.js
+++ HADA/script.js
@@ -3,3 +3,20 @@ $(document).ready(function() {
 		$('ul').toggleClass('active');
 	})
 })
+
+$(function(){
+	khoimonan = $('.allanh').isotope({
+	  itemSelector: '.motanh',
+	  layoutMode: 'masonry'
+	});
+
+	khoimonan.imagesLoaded().progress( function() {
+		khoimonan.isotope('layout');
+	});
+
+	$('.tieudect a').click(function() {
+		dulieu = $(this).data('monan'); 
+		khoimonan.isotope({ filter:  dulieu })
+		 return false ; 
+	});
+})
